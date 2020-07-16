9a36522e0524ec4153c01714efd4e0a876bba2bb
Odritax <odraromina@gmail.com>
2020-07-15 22:12:43 -0400

    primer commit con bonus de localstorage

---
+++ src/components/Contador.vue
@@ -0,0 +1,37 @@
+<template>
+  <div >
+   <h3>Contador de click's</h3>
+   <p>Número de cliks: {{this.cont}}</p>
+   <button  v-on:click="Contar">Contar</button>
+  </div>
+</template>
+
+<script>
+export default {
+        name: "Contador",
+
+    data: function() {
+        return {
+            cont: 0
+        }
+    },
+   mounted: function(){
+     //cuando se monte el codigo es como un document ready
+     const cont_anterior = localStorage.getItem("cont");
+        if (cont_anterior != null) {
+            this.cont = cont_anterior;
+        }
+   },
+  methods: {
+      Contar: function() {
+         this.cont=parseInt(this.cont);
+         this.cont += 1;
+         localStorage.setItem("cont", this.cont);
+        console.log(this.cont);
+      }
+    }
+}
+
+</script>
+<style scoped>
+</style>
