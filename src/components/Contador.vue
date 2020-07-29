8609cfb97fa5bd3712568089f4eb7a38d2bd740f
Odritax <odraromina@gmail.com>
2020-07-29 10:03:57 -0400

    [TAREA] contador-vuex

--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,35 +1,24 @@
 <template>
   <div >
    <h3>Contador de click's</h3>
-   <p>Número de cliks: {{this.cont}}</p>
-   <button  v-on:click="Contar">Contar</button>
+   <p>Número de cliks: {{ cont }}</p>
+   <button v-on:click="Contar()">click!</button>
   </div>
 </template>
 
 <script>
 export default {
-        name: "Contador",
-
-    data: function() {
-        return {
-            cont: 0
-        }
-    },
-   mounted: function(){
-     //cuando se monte el codigo es como un document ready
-     const cont_anterior = localStorage.getItem("cont");
-        if (cont_anterior != null) {
-            this.cont = cont_anterior;
-        }
-   },
-  methods: {
-      Contar: function() {
-         this.cont=parseInt(this.cont);
-         this.cont += 1;
-         localStorage.setItem("cont", this.cont);
-        console.log(this.cont);
+    name: "Contador",
+    methods: {
+      Contar() {
+        this.$store.commit('increment')
       }
+    },
+   computed: {
+    cont() {
+      return this.$store.state.cont
     }
+  }    
 }
 
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import Vuex from 'vuex' // paso 1 importar vuex
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+//llamamos a vuex
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    cont:0
+  },
+  //
+  mutations: {
+    increment(state) {
+      state.cont++
+    }
+  }
+})
+
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
